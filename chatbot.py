44005c5f3bd691fe1ad9711ce30caec63f1a2d73
heba-kaud <hebakaud5@gmail.com>
2026-08-21 22:46:12 +0300

    Normalize user input for chatbot responses

--- chatbot.py
+++ chatbot.py
@@ -9,13 +9,13 @@ while True:
         print("Bot: Hello!😊")
     elif user_input.lower() == "hi":
         print("Bot: Hi! how can i help you?🤖")
-    elif user_input.lower() in ["How are you?", "how r you", "how are u", "how are you", "are you okay?"]:
+    elif user_input.lower().strip("?!.") in ["How are you?", "how r you", "how are u", "how are you", "are you okay?"]:
         print("Bot: I'm fine, thank you!😊")
-    elif user_input.lower() in ["what is your name?", "what's your name","your name"]:
+    elif user_input.lower().strip("?!.") in ["what is your name?", "what's your name","your name"]:
         print("Bot: My name is AI Bot.🤖")
-    elif user_input.lower() == "what is ai?":
+    elif user_input.lower().strip("?!.") == "what is ai?":
         print("Bot: Artifical Intelligance (AI) is the ability of machines to perform taskes that normally require human intelligence. ")
-    elif user_input.lower() == "is there a treatment for seborrheic dermatitis?":
+    elif user_input.lower().strip("?!.") == "is there a treatment for seborrheic dermatitis?":
         print("Bot: Yes, there are treatments that can help control seborrheic eczema. Treatment may include medicated shampoos or creams, depending on the affected area.")
     else:
         print("Bot: Sorry, I don't understand.🤔")
